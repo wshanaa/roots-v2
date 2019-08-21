6efd5da6f67bed60b3375d36be9a9950ce81869b
Wesam Shanaa <wesam.shanaa@axa-equitable.com>
2019-08-20 20:51:21 -0400

    Added 404 page and routing to the app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Route } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Route[] = [
+  {path: '/', pathMatch: 'full', component: AppComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule
